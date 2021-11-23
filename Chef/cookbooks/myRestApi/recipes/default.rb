e29a01456dc281d7a92d8995942c595aa0c02a8c
Eliran Kasif <elirankasif@gmail.com>
2021-11-24 00:29:59 +0200

    add default folder

--- Chef/cookbooks/myRestApi/recipes/default.rb
+++ Chef/cookbooks/myRestApi/recipes/default.rb
@@ -3,6 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
+
+package "python" do 
+  action :install
+end
+
 template "/tmp/app/CloudSchool-DEVOPS/PythonRestApi/code/config" do
   source node["myRestApi"]["config"]
   mode "0644"
