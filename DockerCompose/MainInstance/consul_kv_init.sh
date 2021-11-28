112a987abb48068a004bf4692fc94219c2960e07
Eliran Kasif <elirankasif@gmail.com>
2021-11-28 23:02:27 +0200

    consul client

---
+++ DockerCompose/MainInstance/consul_kv_init.sh
@@ -0,0 +1,8 @@
+consul kv put app/config/LOG_LEVEL "DEBUG"
+consul kv put app/config/END_POINT "free-to-play-games-database.p.rapidapi.com"
+consul kv put app/config/RAPID_API_KEY "fc010fb2aemshe9c83f856d3bae5p183940jsn675a512bbb3a"
+consul kv put app/config/MYSQL_ENDPOINT "cloudschool.cyauuy59rgfa.eu-west-1.rds.amazonaws.com"
+consul kv put app/config/SCHEMA_NAME "alchemy"
+consul kv put app/config/VAULT_ENDPOINT "http://34.241.46.69:8200/"
+consul kv put app/config/VAULT_TOKEN  "myroot"
+consul kv put app/config/VAULT_PATH_TO_CREDS "database/creds/my-role-short"
