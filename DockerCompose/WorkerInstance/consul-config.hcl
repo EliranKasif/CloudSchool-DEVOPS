ee5b8cebe6f0152678266edc0024dfacf4175a7e
Eliran Kasif <elirankasif@gmail.com>
2021-12-01 02:03:15 +0200

    fixed calls for consul template

--- DockerCompose/WorkerInstance/consul-config.hcl
+++ DockerCompose/WorkerInstance/consul-config.hcl
@@ -6,12 +6,12 @@
    password = "test"
  }
 }
- 
+
 log_level = "warn"
 
-# render the role with the new value and re run python application 
+# render the role with the new value and re run python application
 template {
- source = "consul-config-app.tpl"
+ source = "./CloudSchool-DEVOPS/DockerCompose/WorkerInstance/consul-config-app.tpl"
  destination = "./CloudSchool-DEVOPS/PythonRestApi/code/config/app.conf"
   exec {
     command = "python3 ./CloudSchool-DEVOPS/PythonRestApi/code/app.py> ./consul-template.log"
--- DockerCompose/WorkerInstance/env_install.sh
+++ DockerCompose/WorkerInstance/env_install.sh
@@ -23,6 +23,5 @@ wget https://releases.hashicorp.com/consul-template/0.25.2/consul-template_0.25.
 unzip consul-template_0.25.2_linux_amd64.zip
 sudo mv consul-template /usr/local/bin/
 
-consul-template -config="./CloudSchool-DEVOPS/DockerCompose/WorkerInstance" > consul-template.log 2> &1 &
+consul-template -config ./CloudSchool-DEVOPS/DockerCompose/WorkerInstance/consul-config.hcl > consul-template.log 2> &1 &
 
-consul-template -template="/var/www/html/index.html.tpl:/var/www/html/index.html" > consul-template.log 2>&1 &
